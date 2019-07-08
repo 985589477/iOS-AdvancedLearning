91e625cc30ef38e630eb13072da127ca05865831
李璇 <985589477@qq.com>
2019-07-09 00:08:17 +0800

    commit NSObjCRuntime Class Some comments

--- Objective-C/OC-Foundation/OC-Foundation/ViewController.m
+++ Objective-C/OC-Foundation/OC-Foundation/ViewController.m
@@ -1,12 +1,166 @@
-//
-//  ViewController.m
-//  OC-Foundation
-//
-//  Created by 李璇 on 2019/7/8.
-//  Copyright © 2019年 Lxxxxx. All rights reserved.
-//
+/*
+ 总结一下Foundation框架常用的类
+ NSObjCRuntime                           ✅
+ NSAutoreleasePool
+ NSBundle
+ NSArray
+ NSCharacterSet
+ NSCoder
+ NSData
+ NSDate
+ NSCalendar
+ NSDateInterval
+ NSDateFormatter
+ NSDateIntervalFormatter
+ NSISO8601DateFormatter
+ NSMassFormatter
+ NSLengthFormatter
+ NSEnergyFormatter
+ NSMeasurement
+ NSMeasurementFormatter
+ NSPersonNameComponents
+ NSPersonNameComponentsFormatter
+ NSDecimal
+ NSDecimalNumber
+ NSDictionary
+ NSEnumerator
+ NSError
+ NSException
+ NSFileHandle
+ NSFileManager
+ NSFormatter
+ NSHashTable
+ NSHTTPCookie
+ NSHTTPCookieStorage
+ NSIndexPath
+ NSIndexSet
+ NSInvocation
+ NSJSONSerialization
+ NSKeyValueCoding
+ NSKeyValueObserving
+ NSKeyedArchiver
+ NSLocale
+ NSLock
+ NSMapTable
+ NSMethodSignature
+ NSNotification
+ NSNotificationQueue
+ NSNull
+ NSNumberFormatter
+ NSObject
+ NSOperation
+ NSOrderedSet
+ NSOrthography
+ NSPathUtilities
+ NSPointerArray
+ NSPointerFunctions
+ NSPort
+ NSProcessInfo
+ NSPropertyList
+ NSProxy
+ NSRange
+ NSRegularExpression
+ NSRunLoop
+ NSScanner
+ NSSet
+ NSSortDescriptor
+ NSStream
+ NSString
+ NSTextCheckingResult
+ NSThread
+ NSTimeZone
+ NSTimer 
+ NSUnit
+ NSURL
+ NSURLAuthenticationChallenge
+ NSURLCache
+ NSURLConnection
+ NSURLCredential
+ NSURLCredentialStorage
+ NSURLError
+ NSURLProtectionSpace
+ NSURLProtocol
+ NSURLRequest
+ NSURLResponse
+ NSUserDefaults
+ NSValue
+ NSValueTransformer
+ NSXMLParser
+ NSXPCConnection
+ NSZone
+ 
+ NSArchiver
+ NSBackgroundActivityScheduler
+ NSCalendarDate
+ NSConnection
+ NSDistantObject
+ NSDistributedNotificationCenter
+ NSGeometry
+ NSPortCoder
+ NSPortMessage
+ NSPortNameServer
+ NSProtocolChecker
+ NSTask
+ NSXMLDTD
+ NSXMLDTDNode
+ NSXMLDocument
+ NSXMLElement
+ NSXMLNode
+ NSXMLNodeOptions
+ NSURLDownload
+ NSURLHandle
+ 
+ NSAffineTransform
+ NSAppleEventDescriptor
+ NSAppleEventManager
+ NSAppleScript
+ NSClassDescription
+ NSDistributedLock
+ NSGarbageCollector
+ NSHFSFileTypes
+ NSHost
+ NSObjectScripting
+ NSScriptClassDescription
+ NSScriptCoercionHandler
+ NSScriptCommand
+ NSScriptCommandDescription
+ NSScriptExecutionContext
+ NSScriptKeyValueCoding
+ NSScriptObjectSpecifiers
+ NSScriptStandardSuiteCommands
+ NSScriptSuiteRegistry
+ NSScriptWhoseTests
+ NSSpellServer
+ NSUserNotification
+ NSUserScriptTask
+ */
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
+
+//typedef enum : NSUInteger {
+//    ENTypeA = 0,
+//    ENTypeB,
+//    ENTypeC,
+//} ENType;
+
+typedef enum ENType : NSUInteger {
+    ENTypeA = 0,
+    ENTypeB,
+    ENTypeC,
+} ENType;
+
+enum MyType {
+    MyTypeA,
+    MyTypeB,
+    MyTypeC
+};
+
+//enum MyType : NSUInteger {
+//    MyTypeA,
+//    MyTypeB,
+//    MyTypeC
+//};
 
 @interface ViewController ()
 
@@ -16,7 +170,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSUInteger i = MyTypeA;
 }
 
 
